76cf98db94a07c6108cce077bd6c6d3289230c00
Brandon Clarke <ideagasterceo@gmail.com>
2013-10-01 18:06:00 -0400

    Initial Commit w/ JSON Auth, Task & URL's

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authwaketo::Application.config.secret_token = '7f23b772d19538f125b61495e00d7960d9791dedd33ffc38a0252fd799b5910cc5e0c5c43fac038fab4b77cf9ea2bcc5ed3a4c9e7bff069111bbcd3991bab303'
