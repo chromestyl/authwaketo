510d8eba1e0dd40c28f76af1640c6357ee28a7d8
Brandon Clarke <ideagasterceo@gmail.com>
2013-10-02 16:27:48 -0400

    Remove The Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Authwaketo::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
 
   devise_for :users
-  ActiveAdmin.routes(self)
+ # ActiveAdmin.routes(self)
   
   namespace :api do
   namespace :v1 do
