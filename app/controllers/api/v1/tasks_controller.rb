651b47f10aca91dc1e7f636797e00ec1d8ed220e
Brandon Clarke <ideagasterceo@gmail.com>
2013-10-02 16:21:30 -0400

    Removed Tasks Controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::TasksController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  # Just skip the authentication for now
-  # before_filter :authenticate_user!
+  # Authenticate Before Allowing Access
+   before_filter :authenticate_user!
 
   respond_to :json
 
@@ -13,8 +13,8 @@ class Api::V1::TasksController < ApplicationController
   "info":"ok",
   "data":{
           "tasks":[
-                    {"title":"Complete the app"},
-                    {"title":"Complete the tutorial"}
+                    {"title":"My Niggas Bigga Dan Da Bounca"},
+                    {"title":"Dan A Bounca?"}
                   ]
          }
 }'
